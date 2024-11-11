3b839349f0615ff0f64d8202eb4e25e878e9efc2
remolacho <jonathan.rojas@rankmi.com>
2024-11-11 09:20:47 -0500

    init community api

---
+++ src/main/java/com/communities/CommunitiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.communities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitiesApiApplication.class, args);
+	}
+
+}
